8905ae2da66521c1712e69146f8d1218f9dc1033
alexriderspy <sreemantidey1234@gmail.com>
2022-09-15 13:23:24 +0530

    trial

--- A1/solvers.py
+++ A1/solvers.py
@@ -1,3 +1,4 @@
+import string
 class SentenceCorrector(object):
     def __init__(self, cost_fn, conf_matrix):
         self.conf_matrix = conf_matrix
@@ -6,15 +7,38 @@ class SentenceCorrector(object):
         # You should keep updating following variable with best string so far.
         self.best_state = None  
 
+    def get_corr_chars(self,ch):
+        lis = []
+        for letter in string.ascii_lowercase:
+            for chr in self.conf_matrix[letter]:
+                if chr == ch: 
+                    lis.append(letter)
+                    break
+        return lis
+
+    def dfs(self,st,index,min_cost):
+        if index == len(st):
+            return
+        possible_correct_chars = self.get_corr_chars(st[index])
+        if(st[index]==' '):
+            self.dfs(st,index+1,min_cost)
+        for ch in possible_correct_chars:
+            prev_ch = st[index]
+            st[index] = ch
+            cost = self.cost_fn(''.join(st))
+            if cost < min_cost:
+                self.best_state = ''.join(st)
+                min_cost = cost
+                self.dfs(st,index+1,min_cost)
+            st[index]=prev_ch
+
     def search(self, start_state):
         """
         :param start_state: str Input string with spelling errors
         """
         # You should keep updating self.best_state with best string so far.
         # self.best_state = start_state
+        self.best_state = start_state
         cost = self.cost_fn(start_state)
-        end_state = 'the company said president and chief executive officer morris moliver had been chief operating officer as well'
-        print(cost)
-        cost = self.cost_fn(end_state)
-        print(cost)
-        self.best_state = end_state
+        lis = list(start_state)
+        self.dfs(lis,0,10000)
