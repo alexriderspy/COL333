ad41e2f223a5b9b0d03db8310681288ae3c71562
alexriderspy <sreemantidey1234@gmail.com>
2022-10-17 17:50:35 +0530

    errors resolved ai11 use

--- A2/connect4/players/ai9.py
+++ A2/connect4/players/ai9.py
@@ -4,10 +4,10 @@ import numpy as np
 import copy
 from typing import List, Tuple, Dict, Union
 from connect4.utils import get_valid_actions, Integer
-
+import time
 
 class AIPlayer:
-    win_pts = [0, 0, 2, 5, 20]
+    win_pts = [0, 0, 2, 500, 2000]
 
     def __init__(self, player_number: int, time: int):
         """
@@ -19,6 +19,7 @@ class AIPlayer:
         self.player_string = 'Player {}:ai'.format(player_number)
         self.time = time
         self.depth = 4
+        self.best_move = None
         # Do the rest of your implementation here
 
     def get_score(self, player_number: int, row: Union[np.array, List[int]]):
@@ -62,16 +63,16 @@ class AIPlayer:
     def get_pts(self, player_number: int, board: np.array) -> int:
         score = 0
         m, n = board.shape
-        # score in rows
+        
         for i in range(m):
             score += self.get_score(player_number, board[i])
-        # score in columns
+        
         for j in range(n):
             score += self.get_score(player_number, board[:, j])
-        # scores in diagonals_primary
+        
         for diag in self.get_diagonals_primary(board):
             score += self.get_score(player_number, diag)
-        # scores in diagonals_secondary
+        
         for diag in self.get_diagonals_secondary(board):
             score += self.get_score(player_number, diag)
         return score
@@ -98,12 +99,11 @@ class AIPlayer:
         return board, num_popouts
 
     def eval(self,state):
-        return 2*self.get_pts(self.player_number,state[0])-self.get_pts(2 if self.player_number == 1 else 1,state[0])
+        return self.get_pts(self.player_number,state[0])-self.get_pts(2 if self.player_number == 1 else 1,state[0])
 
     def minimax(self, i, state, depth, alpha, beta):
         if i==2:
             if depth >= self.depth or len(get_valid_actions(2 if self.player_number == 1 else 1,state)) == 0:
-
                 return self.eval(state)
             return self.min_val(state,depth,alpha,beta)
         else:
@@ -114,7 +114,8 @@ class AIPlayer:
     def max_val(self,state,depth,alpha,beta):
         maxEval = -inf
         valid_moves = get_valid_actions(self.player_number,state)
-        random.shuffle(valid_moves)
+        valid_moves = self.order_valid_actions(valid_moves)
+
         for move in valid_moves:
             child_state = self.perform_action(self.player_number, move, state)
             maxEval = max(maxEval, self.minimax(2,child_state, depth+1, alpha, beta))
@@ -126,7 +127,8 @@ class AIPlayer:
     def min_val(self,state,depth,alpha,beta):
         minEval = inf
         valid_moves = get_valid_actions(2 if self.player_number == 1 else 1,state)
-        random.shuffle(valid_moves)
+        valid_moves = self.order_valid_actions(valid_moves)
+
         for move in valid_moves:
             child_state = self.perform_action(2 if self.player_number == 1 else 1, move, state)
             minEval = min(minEval, self.minimax(1,child_state, depth+1, alpha, beta))
@@ -137,21 +139,22 @@ class AIPlayer:
 
     def order_valid_actions(self,valid_actions):
         tmp = sorted(valid_actions)
-        l = len(valid_actions)
-        valid_actions = tmp[l//2:] + tmp[0:l//2]
+        no_popouts = list(filter((lambda b : b[1] == False),tmp))
+        popouts = list(filter((lambda b : b[1] == True),tmp))
+        l = len(no_popouts)
+        valid_actions = popouts + no_popouts[l//2:] + no_popouts[0:l//2]
         return valid_actions
 
     def get_intelligent_move(self, state: Tuple[np.array, Dict[int, Integer]]) -> Tuple[int, bool]:
         valid_actions = get_valid_actions(self.player_number,state)
         #order valid actions
-        if len(valid_actions) > 7:
-            valid_actions = self.order_valid_actions(valid_actions)
-        print(valid_actions)
+        
+        valid_actions = self.order_valid_actions(valid_actions)
         if self.player_number == 2:
             if len(valid_actions) >= 8:
                 self.depth = 3
             elif len(valid_actions) >= 4:
-                self.depth = 4
+                self.depth = 5
             else:
                 self.depth = 6
         else:
