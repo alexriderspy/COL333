acee81b14775ba5756c8034fc870490d067ff581
Aryan Dua <cs5200475@iitd.ac.in>
2022-11-14 04:37:50 +0530

    Add files via upload

--- A3/estimator4.py
+++ A3/estimator4.py
@@ -4,6 +4,7 @@ from engine.const import Const
 from math import sin,cos,sqrt
 import random
 import time
+
 # Class: Estimator
 #----------------------
 # Maintain and update a belief distribution over the probability of a car being in a tile.
@@ -38,61 +39,83 @@ class Estimator(object):
     ###################################################################################
     def estimate(self, posX: float, posY: float, observedDist: float, isParked: bool) -> None:
         # BEGIN_YOUR_CODE
-
+        begin = time.time()
         numRows = self.belief.numRows
         numCols = self.belief.numCols
 
         # print(self.transProb)        
         std = Const.SONAR_STD
-
         trans_prob = self.transProb
         
-        #setting the belief according to exact inference
+        #setting the belief according to particle filter
 
-        alpha = 50
-        #beta = 4
-        start = time.time()
-        for _ in range(900):
-            for row in range(numRows):
-                for col in range(numCols):
-                    prob = 1.0
-                    if row+1 < numRows:
-                        if ((row+1,col),(row,col)) in trans_prob:
-                            prob += trans_prob[(row+1,col),(row,col)]*self.belief.getProb(row+1,col)*alpha
-                    if col+1 < numCols:
-                        if ((row,col+1),(row,col)) in trans_prob:
-                            prob += trans_prob[(row,col+1),(row,col)]*self.belief.getProb(row,col+1)*alpha
-                    if row-1 >= 0:
-                        if ((row-1,col),(row,col)) in trans_prob:
-                            prob += trans_prob[(row-1,col),(row,col)]*self.belief.getProb(row-1,col)*alpha
-                    if col-1 >= 0:
-                        if ((row,col-1),(row,col)) in trans_prob:
-                            prob += trans_prob[(row,col-1),(row,col)]*self.belief.getProb(row,col-1)*alpha
-                    
-                    if row+1 < numRows and col+1 < numCols:
-                        if ((row+1,col+1),(row,col)) in trans_prob:
-                            prob += trans_prob[((row+1,col+1),(row,col))]*self.belief.getProb(row+1,col+1)*alpha
-                    if row+1 < numRows and col-1 >=0:
-                        if ((row+1,col-1),(row,col)) in trans_prob:
-                            prob += trans_prob[((row+1,col-1),(row,col))]*self.belief.getProb(row+1,col-1)*alpha
-                    if row-1 >=0 and col+1 < numCols:
-                        if ((row-1,col+1),(row,col)) in trans_prob:
-                            prob += trans_prob[((row-1,col+1),(row,col))]*self.belief.getProb(row-1,col+1)*alpha
-                    if row-1 >= 0 and col-1 >= 0:
-                        if ((row-1,col-1),(row,col)) in trans_prob:
-                            prob += trans_prob[((row-1,col-1),(row,col))]*self.belief.getProb(row-1,col-1)*alpha
-                    
+        #N = numRows*numCols*12
+        N = 10000
+        beliefGrid = self.belief.grid
 
-                    if ((row,col),(row,col)) in trans_prob:
-                        prob += trans_prob[(row,col),(row,col)] * self.belief.getProb(row,col)*alpha
-                    
-                    prob = prob * abs(util.pdf(sqrt((util.colToX(col)-posX)**2 + (util.rowToY(row)-posY)**2),std,observedDist))
-                    #print(prob)
-                    self.belief.setProb(row,col,prob)
+        particles = []
 
-        end = time.time()
-        print(end-start)    
+        for row in range(numRows):
+            for col in range(numCols):
+                number = round(beliefGrid[row][col]*N)
+                for _ in range(number):
+                    particles.append((row,col))
+
+        new_particles = []
+
+        for (x,y) in particles:
+            position = (x,y)
+            adj = [(x,y+1),(x,y-1),(x+1,y),(x-1,y),(x+1,y+1),(x-1,y-1),(x+1,y-1),(x-1,y+1),(x,y)]
+            probs = []
+            for neighbour in adj:
+                if (position, neighbour) in trans_prob.keys():
+                    probs.append(trans_prob[(position, neighbour)])
+                else:
+                    probs.append(0)
+            if max(probs)==0:
+                sampled_particle = [(x,y)]
+            else:
+                sampled_particle = random.choices(adj, weights=probs, k=1)
+            # randomly sample from transitions based on transition probabilities
+            # append only one particle, it cannot duplicate
+            new_particles.append(sampled_particle[0])
+
+        num = len(new_particles)
+
+        weights = [1.]*num
+
+        for i in range(num):
+            (x,y) = new_particles[i]
+            
+            X = util.colToX(y)
+            Y = util.rowToY(x)
+
+            weights[i] = abs(util.pdf(sqrt((X-posX)**2 + (Y-posY)**2),std,observedDist))
+
+        total_sum = 0.0
+        for w in weights:
+            total_sum += w
+
+        particles = random.choices(new_particles, weights=weights, k=N)
+
+
+        dictionary = {}
+
+        for (x,y) in particles:
+            if (x,y) in dictionary:
+                dictionary[(x,y)] = dictionary[(x,y)] + 1
+            else:
+                dictionary[(x,y)] = 1
+        
+        for r in range(numRows):
+            for c in range(numCols):
+                if (r,c) in dictionary:
+                    beliefGrid[r][c] = dictionary[(r,c)]/N
+                else:
+                    beliefGrid[r][c] = 0 
+        self.belief.grid = beliefGrid
         self.belief.normalize()
+        print(time.time() - begin)
         return
   
     def getBelief(self) -> Belief:
